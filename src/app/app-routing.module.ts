656f08046540759baca9762e0f4d01936b49ef35
Dikshitajain <dikshitajaindj20@gmail.com>
2022-01-28 16:46:09 +0530

    bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'product-detail', component: ProductDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
