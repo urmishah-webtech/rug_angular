7e96613518382ea3af5a6dde26066a09ab7ebc70
rahulchoyal <rahul@webtech-evolution.com>
2022-02-11 12:43:30 +0530

    add sections

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,7 +15,11 @@ export class HomeComponent implements OnInit {
   slideConfig = {"slidesToShow": 4, 
   "slidesToScroll": 1,
     "autoplay":true,
-    "speed":1000
-  
+    "speed":1000,
+    "infinite": true,
+  /*  "prevArrow": '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+   "nextArrow": '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+
+  */
   };
 }
