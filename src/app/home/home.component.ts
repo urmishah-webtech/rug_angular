874f6c5d8c881ffa3545e63a6a3013607ff0d847
rahulchoyal <rahul@webtech-evolution.com>
2022-02-15 13:17:27 +0530

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,6 +17,26 @@ export class HomeComponent implements OnInit {
     "autoplay":true,
     "speed":1000,
     "infinite": true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   /*  "prevArrow": '<i class="fa fa-angle-left" aria-hidden="true"></i>',
    "nextArrow": '<i class="fa fa-angle-right" aria-hidden="true"></i>'
 
